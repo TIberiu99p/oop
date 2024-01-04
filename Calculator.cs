b8c44c91d4688fa24f6c946fc17f3b6506a26ab5
Tiberius <Tiberius@DESKTOP-13VJ050>
2024-01-04 14:32:26 +0000

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class Calculator
+    {
+        public int Add(int x, int y) => x + y;
+        public int Subtract(int x, int y) => x - y; 
+        public float Divide(int x, int y) => (float)x / y;
+        public int Multiply(int x, int y) => x * y;
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Calculator calculator = new Calculator();
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int sum = calculator.Add(x, y);
+            int sub = calculator.Subtract(x, y);
+            int mult = calculator.Multiply(x, y);
+            float div = calculator.Divide(x, y);
+            
+            Console.WriteLine($"sum of {x} + {y} = {sum}");
+            Console.WriteLine($"sub of {x} - {y} = {sub}");
+            Console.WriteLine($"mult of {x} * {y} = {mult}");
+            Console.WriteLine($"div of {x} / {y} = {div.ToString("F2")}");
+            Console.ReadLine();
+            
+            
+
+        }
+    }
+}
